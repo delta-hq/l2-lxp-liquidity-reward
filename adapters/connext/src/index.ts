0af98697071a677a231c75aa09da4170d4e48866
Layne Haber <layne.haber@gmail.com>
2024-03-21 12:27:50 -0600

    feat: working script

--- adapters/connext/src/index.ts
+++ adapters/connext/src/index.ts
@@ -1,4 +1,22 @@
-const main = () => {
-    console.log('Hello, world!');
+import { getUserTVLByBlock, writeCsv } from "./utils";
+
+const input = {
+    blockNumber: 2954869,
+    blockTimestamp: 1711044141,
 }
-main();
+
+const fileName = 'output.csv';
+console.log('Getting TVL at block:', input.blockNumber);
+
+
+// returns all user balances at the input block by looking at the latest
+// balance for each user and token on the subgraph, capped at given block.
+getUserTVLByBlock(input).then((data) => {
+    if (data.length === 0) {
+        console.log("no data to write to file");
+        return;
+    }
+    writeCsv(data, fileName).then(() => {
+        console.log('CSV written to file:', fileName);
+    })
+});
