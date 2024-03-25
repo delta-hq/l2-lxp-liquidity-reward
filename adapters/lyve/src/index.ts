837bbc89b078d882043566a9590fada8e7dd114d
LyveFinance <alsudayes@gmail.com>
2024-03-25 16:39:09 +0800

    Add Lyve Finance Adapter
    Add Lyve Finance Adapter

--- adapters/lyve/src/index.ts
+++ adapters/lyve/src/index.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import * as fs from "fs";
 import { write } from "fast-csv";
 
 
@@ -7,12 +7,24 @@ type OutputDataSchemaRow = {
     timestamp: number;
     user_address: string;
     token_address: string;
-    token_balance: bigint;
+    token_balance: number;
+    token_symbol: string;
+    usd_price: number;
 };
 
 const LINEA_RPC = "https://rpc.linea.build";
+const LYU_ADDRESS = "0xb20116eE399f15647BB1eEf9A74f6ef3b58bc951";
 
 const LYVE_SUBGRAPH_QUERY_URL = "https://api.studio.thegraph.com/query/53783/lyve-lp-tvl/version/latest";
+
+const LYVE_STABILITY_POOL_QUERY = `
+    query StabilityPoolQuery {
+        userDeposits(first: 1000,orderBy: _newDeposit, orderDirection: desc) {
+            _depositor,
+            _newDeposit
+        }
+    }
+`;
 
 const _VESSELS_QUERY = `
     query VesselQuery {
@@ -40,19 +52,22 @@ const post = async (url: string, data: any): Promise<any> => {
     return await response.json();
 };
 
-const getLatestBlockNumberAndTimestamp = async () => {
-    const data = await post(LINEA_RPC, {
-        jsonrpc: "2.0",
-        method: "eth_getBlockByNumber",
-        params: ["latest", false],
-        id: 1,
-    });
-    const blockNumber = parseInt(data.result.number);
-    const blockTimestamp = parseInt(data.result.timestamp);
-    return { blockNumber, blockTimestamp };
+const getStabilityPoolData = async (blockNumber: number, blockTimestamp: number): Promise<OutputDataSchemaRow[]> => {
+    const csvRows: OutputDataSchemaRow[] = [];
+    const responseJson = await post(LYVE_SUBGRAPH_QUERY_URL, { query: LYVE_STABILITY_POOL_QUERY });
+    for (const item of responseJson.data.userDeposits) {
+        csvRows.push({
+            block_number: blockNumber,
+            timestamp: blockTimestamp,
+            user_address: item._depositor,
+            token_address: LYU_ADDRESS,
+            token_balance: item._newDeposit,
+            token_symbol: "LYU",
+            usd_price: 0
+        });
+    }
+    return csvRows;
 };
-
-
 const getVesselDepositsData = async (blockNumber: number, blockTimestamp: number): Promise<OutputDataSchemaRow[]> => {
     const csvRows: OutputDataSchemaRow[] = [];
     const responseJson = await post(LYVE_SUBGRAPH_QUERY_URL, { query: _VESSELS_QUERY });
@@ -62,26 +77,62 @@ const getVesselDepositsData = async (blockNumber: number, blockTimestamp: number
         csvRows.push({
             block_number: blockNumber,
             timestamp: blockTimestamp,
+            user_address: item._borrower,
             token_address: item._asset,
             token_balance: updatedAssetAmount,
-            user_address: item._borrower,
+            token_symbol: "",
+            usd_price: 0
         });
     }
     return csvRows;
 };
+interface BlockData {
+    blockNumber: number;
+    blockTimestamp: number;
+}
+export const main = async (blocks: BlockData[]) => {
+    const allCsvRows: any[] = []; // Array to accumulate CSV rows for all blocks
+    const batchSize = 10; // Size of batch to trigger writing to the file
+    let i = 0;
 
-const main = async () => {
-    const { blockNumber, blockTimestamp } = await getLatestBlockNumberAndTimestamp();
-    const csvRowsVessels = await getVesselDepositsData(blockNumber, blockTimestamp);
-    // Write the CSV output to a file
-    const ws = fs.createWriteStream("outputData.csv");
-    write(csvRowsVessels, { headers: true })
-        .pipe(ws)
-        .on("finish", () => {
-            console.log("CSV file has been written.");
-        });
+    for (const { blockNumber, blockTimestamp } of blocks) {
+        try {
+            // Retrieve data using block number and timestamp
+            const csvRowsStabilityPool = await getStabilityPoolData(blockNumber, blockTimestamp);
+            const csvRowsVessels = await getVesselDepositsData(blockNumber, blockTimestamp);
+            const csvRows = csvRowsStabilityPool.concat(csvRowsVessels);
+
+            // Accumulate CSV rows for all blocks
+            allCsvRows.push(...csvRows);
+
+            i++;
+            console.log(`Processed block ${i}`);
+
+            // Write to file when batch size is reached or at the end of loop
+            if (i % batchSize === 0 || i === blocks.length) {
+                const ws = fs.createWriteStream(`outputData.csv`, { flags: i === batchSize ? 'w' : 'a' });
+                write(allCsvRows, { headers: i === batchSize ? true : false })
+                    .pipe(ws)
+                    .on("finish", () => {
+                        console.log(`CSV file has been written.`);
+                    });
+
+                // Clear the accumulated CSV rows
+                allCsvRows.length = 0;
+            }
+        } catch (error) {
+            console.error(`An error occurred for block ${blockNumber}:`, error);
+        }
+    }
 };
 
-main().then(() => {
-    console.log("Done");
-});
+export const getUserTVLByBlock = async (blocks: BlockData) => {
+    const { blockNumber, blockTimestamp } = blocks
+    //    Retrieve data using block number and timestamp
+    const csvRowsStabilityPool = await getStabilityPoolData(blockNumber, blockTimestamp);
+    const csvRowsVessels = await getVesselDepositsData(blockNumber, blockTimestamp);
+    const csvRows = csvRowsStabilityPool.concat(csvRowsVessels);
+    return csvRows
+};
+// 调用 main 函数
+main([{ blockNumber: 3123010, blockTimestamp: 1711354844 }]).catch(console.error);
