7dd25fd94971d8c9e50d5a4dfca83e92c09b217e
Ramon <ramon@spectral.finance>
2024-04-14 19:47:21 -0300

    update

--- adapters/lynex/src/index.ts
+++ adapters/lynex/src/index.ts
@@ -8,6 +8,7 @@ import {
 } from "./sdk/lensDetails";
 import BigNumber from "bignumber.js";
 import { BlockData, OutputSchemaRow } from "./sdk/types";
+import csv from 'csv-parser';
 import {
   getV2UserPositionsAtBlock,
   getV3UserPositionsAtBlock,
@@ -196,6 +197,68 @@ export const getUserLiquidityTVLByBlock = async ({
   return result;
 };
 
-getData().then(() => {
-  console.log("Done");
+// getData().then(() => {
+//   console.log("Done");
+// });
+
+const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
+  const blocks: BlockData[] = [];
+
+  await new Promise<void>((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv()) // Specify the separator as '\t' for TSV files
+      .on('data', (row) => {
+        const blockNumber = parseInt(row.number, 10);
+        const blockTimestamp = parseInt(row.timestamp, 10);
+        if (!isNaN(blockNumber) && blockTimestamp) {
+          blocks.push({ blockNumber: blockNumber, blockTimestamp });
+        }
+      })
+      .on('end', () => {
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+
+  return blocks;
+};
+
+readBlocksFromCSV('src/hourly_blocks.csv').then(async (blocks) => {
+  console.log(blocks);
+  const allCsvRows: any[] = []; // Array to accumulate CSV rows for all blocks
+  const batchSize = 1000; // Size of batch to trigger writing to the file
+  let i = 0;
+
+  allCsvRows.push({ block_number: 'block_number', timestamp: 'timestamp', user_address: 'user_address', token_address: 'token_address', token_balance: 'token_balance', token_symbol: 'token_symbol', usd_price: 'usd_price' });
+  for (const block of blocks) {
+      try {
+          const result = await getUserTVLByBlock(block);
+
+          // Accumulate CSV rows for all blocks
+          allCsvRows.push(...result);
+
+          i++;
+          // console.log(`Processed block ${i}`);
+
+          // Write to file when batch size is reached or at the end of loop
+          if (i % batchSize === 0 || i === blocks.length) {
+              const ws = fs.createWriteStream(`outputData.csv`, { flags: i === batchSize ? 'w' : 'a' });
+              write(allCsvRows, { headers: i === batchSize ? true : false })
+                  .pipe(ws)
+                  .on("finish", () => {
+                  console.log(`CSV file has been written.`);
+                  });
+
+              // Clear the accumulated CSV rows
+              allCsvRows.length = 0;
+          }
+      } catch (error) {
+          console.error(`An error occurred for block ${block}:`, error);
+      }
+
+  }
+  }).catch((err) => {
+  console.error('Error reading CSV file:', err);
 });
