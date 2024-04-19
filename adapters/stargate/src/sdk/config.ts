1aeaf1949bdc17b0afeb517d3d91520e4c40cf99
kyle-layerzero <98773402+kyle-layerzero@users.noreply.github.com>
2024-04-18 22:45:01 -0700

    feat: stargate adapter

---
+++ adapters/stargate/src/sdk/config.ts
@@ -0,0 +1,10 @@
+import { createPublicClient, http } from "viem";
+import { linea } from "viem/chains";
+
+export const SUBGRAPH_URL =
+  "https://api.studio.thegraph.com/query/42109/linea-balances/version/latest";
+
+export const client = createPublicClient({
+  chain: linea,
+  transport: http("https://rpc.linea.build"),
+});
