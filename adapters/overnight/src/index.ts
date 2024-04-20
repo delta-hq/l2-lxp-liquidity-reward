7d40a68a572371ed1d24c5455f086e2e0025c4b6
matveyb <matvey.b2022@gmail.com>
2024-04-20 23:38:39 +0200

    rebase done

--- adapters/overnight/src/index.ts
+++ adapters/overnight/src/index.ts
@@ -1,5 +1,5 @@
-import { CHAINS, LP_LYNEX_SYMBOL, LP_LYNEX, PROTOCOLS, SNAPSHOTS_BLOCKS } from "./sdk/config";
-import { getLPValueByUserAndPoolFromPositions, getPositionsForAddressByPoolAtBlock, getTimestampAtBlock } from "./sdk/subgraphDetails";
+import { CHAINS, PROTOCOLS, SNAPSHOTS_BLOCKS, OVN_CONTRACTS, LP_LYNEX, LP_LYNEX_SYMBOL, USD_PLUS_SYMBOL, USD_PLUS_LINEA, USDT_PLUS_SYMBOL, USDT_PLUS_LINEA } from "./sdk/config";
+import { Position, getLPValueByUserAndPoolFromPositions, getPositionsForAddressByPoolAtBlock, getRebaseForUsersByPoolAtBlock, getTimestampAtBlock } from "./sdk/subgraphDetails";
 
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
@@ -7,6 +7,7 @@ import { getLPValueByUserAndPoolFromPositions, getPositionsForAddressByPoolAtBlo
 
 import fs from 'fs';
 import { write } from 'fast-csv';
+import BN from "bignumber.js";
 
 interface CSVRow {
   block_number: string;
@@ -18,39 +19,87 @@ interface CSVRow {
 }
 
 const getData = async () => {
-  const csvRows: CSVRow[] = [];
-
+  const csvRows_pools: CSVRow[] = [];
+  const csvRows_rebase: CSVRow[] = [];
+  
   for (let block of SNAPSHOTS_BLOCKS) {
     const positions = await getPositionsForAddressByPoolAtBlock(
       block, "", "", CHAINS.LINEA, PROTOCOLS.OVN
     );
-    
-    console.log(`Block: ${block}`);
     console.log("Positions: ", positions.length);
-
     let lpValueByUsers = getLPValueByUserAndPoolFromPositions(positions);
-
+    // all results are counted for the END block
     const timestamp = new Date(await getTimestampAtBlock(block)).toISOString();
 
     lpValueByUsers.forEach((value, key) => {
       value.forEach((lpValue) => {
-        const lpValueStr = lpValue.toString();
-        // Accumulate CSV row data
-        csvRows.push({
-          user_address: key,
-          token_address: LP_LYNEX,
-          token_symbol: LP_LYNEX_SYMBOL,
-          token_balance: lpValueStr,
-          block_number: block.toString(),
-          timestamp
+          const lpValueStr = lpValue.toString();
+          // Accumulate CSV row data
+          csvRows_pools.push({
+            user_address: key,
+            token_address: LP_LYNEX,
+            token_symbol: LP_LYNEX_SYMBOL,
+            token_balance: lpValueStr,
+            block_number: block.toString(),
+            timestamp
         });
+      })
+    });
+  }
+
+  for (let [index, block] of SNAPSHOTS_BLOCKS.entries()) {
+    console.log(`Block: ${block}`);
+    let positionsRebaseUsd: Map<string, string> = new Map()
+    let positionsRebaseUsdt: Map<string, string> = new Map()
+    if (!SNAPSHOTS_BLOCKS[index + 1]) return
+    console.log(block, '__index')
+    if (SNAPSHOTS_BLOCKS[index + 1]) {
+      positionsRebaseUsd = await getRebaseForUsersByPoolAtBlock(
+        block, SNAPSHOTS_BLOCKS[index + 1], CHAINS.LINEA, PROTOCOLS.OVN_REBASE, OVN_CONTRACTS.USDPLUS
+      );
+    }
+    if (SNAPSHOTS_BLOCKS[index + 1]) {
+      positionsRebaseUsdt = await getRebaseForUsersByPoolAtBlock(
+        block, SNAPSHOTS_BLOCKS[index + 1], CHAINS.LINEA, PROTOCOLS.OVN_REBASE, OVN_CONTRACTS.USDTPLUS
+      );
+    }
+    console.log(`Block: ${block}`);
+    console.log("positionsRebase: ", positionsRebaseUsd.size);
+
+    // all results are counted for the END block
+    const timestamp = new Date(await getTimestampAtBlock(SNAPSHOTS_BLOCKS[index + 1])).toISOString();
+
+
+    positionsRebaseUsd.forEach((value, key) => {
+      csvRows_rebase.push({
+        user_address: key,
+        token_symbol: USD_PLUS_SYMBOL,
+        token_balance: value,
+        token_address: USD_PLUS_LINEA,
+        block_number: SNAPSHOTS_BLOCKS[index + 1].toString(),
+        timestamp
+      });
+    });
+
+    positionsRebaseUsdt.forEach((value, key) => {
+      csvRows_rebase.push({
+        user_address: key,
+        token_symbol: USDT_PLUS_SYMBOL,
+        token_balance: value,
+        token_address: USDT_PLUS_LINEA,
+        block_number: SNAPSHOTS_BLOCKS[index + 1].toString(),
+        timestamp
       });
     });
   }
 
   // Write the CSV output to a file
-  const ws = fs.createWriteStream('outputData.csv');
-  write(csvRows, { headers: true }).pipe(ws).on('finish', () => {
+  const ws = fs.createWriteStream('outputData_pools.csv');
+  const ws_rebase = fs.createWriteStream('outputData_rebase.csv');
+  write(csvRows_pools, { headers: true }).pipe(ws).on('finish', () => {
+    console.log("CSV file has been written.");
+  });
+  write(csvRows_rebase, { headers: true }).pipe(ws_rebase).on('finish', () => {
     console.log("CSV file has been written.");
   });
 };
--- adapters/overnight/src/sdk/config.ts
+++ adapters/overnight/src/sdk/config.ts
@@ -2,29 +2,42 @@ export const enum CHAINS{
   LINEA = 59144,
 }
 export const enum PROTOCOLS{
-  OVN = 1
+  OVN = 1,
+  OVN_REBASE = 2
 }
 
 export const enum AMM_TYPES{
   UNISWAPV3 = 0,
 }
 
-export const enum OVNPOOLS{
+export const enum OVN_CONTRACTS{
   FIRST = 0,
   SECOND = 1,
+  USDPLUS = 2,
+  USDTPLUS = 3,
 }
 
 export const SUBGRAPH_URLS = {
   [CHAINS.LINEA]: {
+      [PROTOCOLS.OVN_REBASE]: {
+        [OVN_CONTRACTS.USDPLUS]: {
+            url:  "https://api.studio.thegraph.com/query/68020/ovn_linea_points/version/latest",
+            address: "0xB79DD08EA68A908A97220C76d19A6aA9cBDE4376"
+        },
+        [OVN_CONTRACTS.USDTPLUS]: {
+            url:  "https://api.studio.thegraph.com/query/68020/ovn_linea_points_usdt/version/latest",
+            address: "0x1E1F509963A6D33e169D9497b11c7DbFe73B7F13"
+        },
+      },
       [PROTOCOLS.OVN]: {
-          [OVNPOOLS.FIRST]: {
-              url:  "https://api.studio.thegraph.com/query/68020/linea_ovn/version/latest",
-              pool: "0xc5f4c5c2077bbbac5a8381cf30ecdf18fde42a91"
-          },
-          [OVNPOOLS.SECOND]: {
-              url:  "https://api.studio.thegraph.com/query/68020/ovn_linea_2/version/latest",
-              pool: "0x58aacbccaec30938cb2bb11653cad726e5c4194a"
-          } 
+        [OVN_CONTRACTS.FIRST]: {
+            url:  "https://api.studio.thegraph.com/query/68020/linea_ovn/version/latest",
+            address: "0xc5f4c5c2077bbbac5a8381cf30ecdf18fde42a91"
+        },
+        [OVN_CONTRACTS.SECOND]: {
+            url:  "https://api.studio.thegraph.com/query/68020/ovn_linea_2/version/latest",
+            address: "0x58aacbccaec30938cb2bb11653cad726e5c4194a"
+        },
       }
   }
 }
@@ -34,11 +47,23 @@ export const RPC_URLS = {
 };
 
 
+// export const SNAPSHOTS_BLOCKS = [
+//     2361808, 2505008, 2548208, 2591408, 2677808,
+//     2721008, 2764208, 2807408, 2850608, 2893808,
+//     2953808, 3020000
+// ];
+
+
 export const SNAPSHOTS_BLOCKS = [
-    2361808, 2505008, 2548208, 2591408, 2677808,
-    2721008, 2764208, 2807408, 2850608, 2893808,
-    2953808, 3020000
+  2361808, 3020000, 3867401
 ];
 
+export const LINEA_RPC = "https://lb.drpc.org/ogrpc?network=linea&dkey=AsCWb9aYukugqNphr9pEGw5L893HadYR7ooVbrjxQOzW"
 export const LP_LYNEX_SYMBOL = "oLYNX";
 export const LP_LYNEX = "0x63349BA5E1F71252eCD56E8F950D1A518B400b60"
+
+export const USD_PLUS_SYMBOL = "usd+"
+export const USDT_PLUS_SYMBOL = "usdt+"
+export const USD_PLUS_LINEA = "0xB79DD08EA68A908A97220C76d19A6aA9cBDE4376"
+export const USDT_PLUS_LINEA = "0x1E1F509963A6D33e169D9497b11c7DbFe73B7F13"
+export const ZERO_ADD = "0x0000000000000000000000000000000000000000";
