f1b4752438c4a8c8ea31bf1317d0d134f44d1c01
Jhon Lucas <jhonlucaslife@gmail.com>
2024-04-22 18:23:05 -0300

    refactor: change script to receive a csv as input and the way to write the output.

--- adapters/overnight/src/index.ts
+++ adapters/overnight/src/index.ts
@@ -1,28 +1,47 @@
-import { CHAINS, LP_LYNEX_SYMBOL, LP_LYNEX, PROTOCOLS, SNAPSHOTS_BLOCKS } from "./sdk/config";
+import csv from 'csv-parser';
+import fs from 'fs';
+import { write } from 'fast-csv';
+import { CHAINS, LP_LYNEX_SYMBOL, LP_LYNEX, PROTOCOLS } from "./sdk/config";
+import { BlockData} from './sdk/types';
 import { getLPValueByUserAndPoolFromPositions, getPositionsForAddressByPoolAtBlock, getTimestampAtBlock } from "./sdk/subgraphDetails";
 
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
 };
 
-import fs from 'fs';
-import { write } from 'fast-csv';
+const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
+  const blocks: BlockData[] = [];
 
-interface CSVRow {
-  block_number: string;
-  timestamp: string;
-  user_address: string;
-  token_address: string;
-  token_balance: string;
-  token_symbol: string;
-}
+  await new Promise<void>((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv()) // Specify the separator as '\t' for TSV files
+      .on('data', (row) => {
+        const blockNumber = parseInt(row.number, 10);
+        const blockTimestamp = parseInt(row.timestamp, 10);
+        if (!isNaN(blockNumber) && blockTimestamp) {
+          blocks.push({ blockNumber: blockNumber, blockTimestamp });
+        }
+      })
+      .on('end', () => {
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
 
-const getData = async () => {
-  const csvRows: CSVRow[] = [];
+  return blocks;
+};
 
-  for (let block of SNAPSHOTS_BLOCKS) {
+readBlocksFromCSV('hourly_blocks.csv').then(async (blocks: any[]) => {
+  console.log(blocks);
+  const allCsvRows: any[] = []; // Array to accumulate CSV rows for all blocks
+  const batchSize = 1000; // Size of batch to trigger writing to the file
+  let i = 0;
+
+  for (let block of blocks) {
     const positions = await getPositionsForAddressByPoolAtBlock(
-      block, "", "", CHAINS.LINEA, PROTOCOLS.OVN
+      block.blockNumber, "", "", CHAINS.LINEA, PROTOCOLS.OVN
     );
     
     console.log(`Block: ${block}`);
@@ -30,32 +49,37 @@ const getData = async () => {
 
     let lpValueByUsers = getLPValueByUserAndPoolFromPositions(positions);
 
-    const timestamp = new Date(await getTimestampAtBlock(block)).toISOString();
+    const timestamp = new Date(await getTimestampAtBlock(block.blockNumber)).toISOString();
 
     lpValueByUsers.forEach((value, key) => {
       value.forEach((lpValue) => {
         const lpValueStr = lpValue.toString();
         // Accumulate CSV row data
-        csvRows.push({
+        allCsvRows.push({
           user_address: key,
           token_address: LP_LYNEX,
           token_symbol: LP_LYNEX_SYMBOL,
           token_balance: lpValueStr,
-          block_number: block.toString(),
+          block_number: block.blockNumber.toString(),
           timestamp
         });
       });
     });
   }
 
-  // Write the CSV output to a file
-  const ws = fs.createWriteStream('outputData.csv');
-  write(csvRows, { headers: true }).pipe(ws).on('finish', () => {
-    console.log("CSV file has been written.");
+  await new Promise((resolve, reject) => {
+    // const randomTime = Math.random() * 1000;
+    // setTimeout(resolve, randomTime);
+    const ws = fs.createWriteStream(`outputData.csv`, { flags: 'w' });
+    write(allCsvRows, { headers: true })
+        .pipe(ws)
+        .on("finish", () => {
+        console.log(`CSV file has been written.`);
+        resolve;
+        });
   });
-};
 
-getData().then(() => {
-  console.log("Done");
+}).catch((err) => {
+  console.error('Error reading CSV file:', err);
 });
 
