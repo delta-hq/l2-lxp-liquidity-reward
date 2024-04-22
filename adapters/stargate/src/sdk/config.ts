3a3a7a709beb1cbd894db55d66d0d4e6091e51fe
kyle-layerzero <98773402+kyle-layerzero@users.noreply.github.com>
2024-04-22 13:54:37 -0700

    update subgraph

--- adapters/stargate/src/sdk/config.ts
+++ adapters/stargate/src/sdk/config.ts
@@ -2,7 +2,7 @@ import { createPublicClient, http } from "viem";
 import { linea } from "viem/chains";
 
 export const SUBGRAPH_URL =
-  "https://api.studio.thegraph.com/query/42109/linea-balances/version/latest";
+  "https://api.studio.thegraph.com/query/72458/linea-balances/version/latest";
 
 export const client = createPublicClient({
   chain: linea,
