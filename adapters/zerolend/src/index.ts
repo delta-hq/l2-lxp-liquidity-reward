fa37361f3ccae580908b139cff4faaa16fbb5c45
Deadshot Ryker <ryker@zerolend.xyz>
2024-04-29 22:44:27 +0300

    Got zerolend balances of atoken and debt tokens to be captured properly.

--- adapters/zerolend/src/index.ts
+++ adapters/zerolend/src/index.ts
@@ -1,19 +1,21 @@
-import { ethers } from "ethers";
 import { write } from "fast-csv";
 import axios from "axios";
 import fs from "fs";
 
-const lineaProvider = new ethers.JsonRpcProvider("https://rpc.linea.build	");
-
 interface IResponse {
   data: {
     userReserves: {
-      supplyBalance: string;
-      debtBalance: string;
-      reserve: string;
       user: {
         id: string;
       };
+      currentTotalDebt: string;
+      currentATokenBalance: string;
+      reserve: {
+        underlyingAsset: string;
+        symbol: string;
+        name: string;
+      };
+      liquidityRate: "0";
     }[];
   };
 }
@@ -34,17 +36,8 @@ interface BlockData {
 }
 
 const queryURL =
-  "https://api.studio.thegraph.com/query/65585/zl-linea-points/version/latest";
+  "https://api.studio.thegraph.com/query/65585/zerolend-linea-market/version/latest";
 
-const symbolMapping: { [key: string]: string } = {
-  "0xe5d7c2a44ffddf6b295a15c148167daaaf5cf34f": "WETH",
-  "0xf3b001d64c656e30a62fbaaca003b1336b4ce12a": "MAI",
-  "0x176211869ca2b568f2a7d4ee941e073a821ee1ff": "USDC",
-  "0xa219439258ca9da29e9cc4ce5596924745e12b93": "USDT",
-  "0x2416092f143378750bb29b79ed961ab195cceea5": "ezETH",
-  "0x894134a25a5fac1c2c26f1d8fbf05111a3cb9487": "GRAI",
-  "0x3aab2285ddcddad8edf438c1bab47e1a9d05a9b4": "WBTC",
-};
 export const main = async (
   blockNumber: number,
   blockTimestamp: number
@@ -56,41 +49,51 @@ export const main = async (
   const rows: OutputDataSchemaRow[] = [];
 
   do {
-    const query = `query {
-      userReserves(where: {id_gt: "${lastAddress}"}, first: ${first}) {
-        supplyBalance
-        debtBalance
-        reserve
+    const query = `{
+      userReserves(
+        where: {and: [{or: [{currentTotalDebt_gt: 0}, {currentATokenBalance_gt: 0}]}, {user_gt: "${lastAddress}"}]}
+        first: ${first}
+      ) {
         user {
           id
         }
+        currentTotalDebt
+        currentATokenBalance
+        reserve {
+          underlyingAsset
+          symbol
+          name
+        }
+        liquidityRate
       }
     }`;
 
     const headers = {
       "Content-Type": "application/json",
     };
+
     const batch = await axios.post<IResponse>(queryURL, { query }, { headers });
     if (batch.data.data.userReserves.length <= 1) break;
 
     batch.data.data.userReserves.forEach((data) => {
-      const balance = BigInt(data.supplyBalance) - BigInt(data.debtBalance);
+      const balance =
+        BigInt(data.currentATokenBalance) - BigInt(data.currentTotalDebt);
 
       if (balance !== 0n)
         rows.push({
           block_number: blockNumber,
           timestamp: blockTimestamp,
           user_address: data.user.id,
-          token_address: data.reserve,
+          token_address: data.reserve.underlyingAsset,
           token_balance: Number(balance),
-          token_symbol: symbolMapping[data.reserve.toLowerCase()] || "",
+          token_symbol: data.reserve.symbol,
           usd_price: 0,
         });
 
       lastAddress = data.user.id;
     });
 
-    console.log(`Processed ${rows.length} rows`);
+    console.log(`Processed ${rows.length} rows`, lastAddress);
   } while (true);
 
   return rows;
@@ -105,6 +108,7 @@ export const writeCSV = async (data: OutputDataSchemaRow[]) => {
     "user_address",
     "token_address",
     "token_balance",
+    "token_symbol",
   ];
 
   // Create a write stream
@@ -122,6 +126,7 @@ export const writeCSV = async (data: OutputDataSchemaRow[]) => {
       row.user_address,
       row.token_address,
       row.token_balance,
+      row.token_symbol,
     ]);
   });
 
