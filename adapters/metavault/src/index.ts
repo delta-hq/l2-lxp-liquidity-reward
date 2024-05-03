8428689f4c3d4556e64d4253503a46a63dbca0cb
Ramon <ramon@spectral.finance>
2024-05-03 14:46:15 -0300

    update

--- adapters/metavault/src/index.ts
+++ adapters/metavault/src/index.ts
@@ -3,6 +3,9 @@ import { getPositionDetailsFromPosition, getPositionsForAddressByPoolAtBlock, ge
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
 };
+import { write } from 'fast-csv';
+import csv from 'csv-parser';
+import fs from 'fs';
 
 interface BlockData {
   blockNumber: number;
@@ -89,3 +92,54 @@ export const getPoolData = async ({
   }
   return allCsvRows
 }
+
+const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
+  const blocks: BlockData[] = [];
+
+  await new Promise<void>((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv()) // Specify the separator as '\t' for TSV files
+      .on('data', (row) => {
+        const blockNumber = parseInt(row.number, 10);
+        const blockTimestamp = parseInt(row.timestamp, 10);
+        if (!isNaN(blockNumber) && blockTimestamp) {
+          blocks.push({ blockNumber: blockNumber, blockTimestamp });
+        }
+      })
+      .on('end', () => {
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+
+  return blocks;
+};
+
+readBlocksFromCSV('hourly_blocks.csv').then(async (blocks: BlockData[]) => {
+  console.log(blocks);
+  const allCsvRows: any[] = []; // Array to accumulate CSV rows for all blocks
+
+  for (const block of blocks) {
+      try {
+          const result = await getUserTVLByBlock(block);
+          for(let i = 0; i < result.length; i++){
+              allCsvRows.push(result[i])
+          }
+      } catch (error) {
+          console.error(`An error occurred for block ${block}:`, error);
+      }
+  }
+  await new Promise((resolve, reject) => {
+    const ws = fs.createWriteStream(`outputData.csv`, { flags: 'w' });
+    write(allCsvRows, { headers: true })
+        .pipe(ws)
+        .on("finish", () => {
+        console.log(`CSV file has been written.`);
+        resolve;
+        });
+  });
+}).catch((err) => {
+  console.error('Error reading CSV file:', err);
+});
