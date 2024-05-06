463fe69085ca5d5f6610c1852142e247f957c09a
gathonwar <main@sonne.finance>
2024-05-06 18:19:24 +0300

    fix(mendi): use new subgraph for performance

--- adapters/mendi/src/sdk/config.ts
+++ adapters/mendi/src/sdk/config.ts
@@ -9,7 +9,7 @@ export const enum PROTOCOLS {
 export const SUBGRAPH_URLS = {
   [CHAINS.LINEA]: {
     [PROTOCOLS.MENDI]: {
-      url: "https://api.goldsky.com/api/public/project_cltshpix6kkj301x1b4ilh6pm/subgraphs/mendi-linea/1.4/gn",
+      url: "https://api.goldsky.com/api/public/project_cltshpix6kkj301x1b4ilh6pm/subgraphs/mendi-subgraph/1.0/gn",
     },
   },
 };
