0a843aa6d0f4c2cf07ba4b595517c6b861eaf77d
auralshin <anshulspartan141@gmail.com>
2024-05-11 11:26:57 +0530

    Changes as per PR requirements

--- adapters/renzo/src/index.ts
+++ adapters/renzo/src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import { format, write } from "fast-csv";
+import csv from "csv-parser";
 require("dotenv").config();
-
 
 const excludedAddresses = new Set([
   "0x8a90d208666deec08123444f67bf5b1836074a67", // Mendi
@@ -88,6 +88,58 @@ export const getUserTVLByBlock = async (
     return [];
   }
 };
+
+const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
+  const blocks: BlockData[] = [];
+
+  await new Promise<void>((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv()) // Specify the separator as '\t' for TSV files
+      .on("data", (row) => {
+        const blockNumber = parseInt(row.number, 10);
+        const blockTimestamp = parseInt(row.timestamp, 10);
+        if (!isNaN(blockNumber) && blockTimestamp) {
+          blocks.push({ blockNumber: blockNumber, blockTimestamp });
+        }
+      })
+      .on("end", () => {
+        resolve();
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+
+  return blocks;
+};
+
+readBlocksFromCSV("hourly_blocks.csv")
+  .then(async (blocks: any[]) => {
+    console.log(blocks);
+    // const allCsvRows: any[] = [];
+
+    // for (const block of blocks) {
+    //   try {
+    //     const result = await getUserTVLByBlock(block);
+    //     allCsvRows.push(...result);
+    //   } catch (error) {
+    //     console.error(`An error occurred for block ${block}:`, error);
+    //   }
+    // }
+    // await new Promise((resolve, reject) => {
+    //   const ws = fs.createWriteStream(`outputData.csv`, { flags: "w" });
+    //   write(allCsvRows, { headers: true })
+    //     .pipe(ws)
+    //     .on("finish", () => {
+    //       console.log(`CSV file has been written.`);
+    //       resolve;
+    //     });
+    // });
+    await main(blocks);
+  })
+  .catch((err) => {
+    console.error("Error reading CSV file:", err);
+  });
 
 export const main = async (blocks: BlockData[]) => {
   // Open a write stream for the unified output file.
