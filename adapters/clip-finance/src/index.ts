ba6ca0f65a8811d2ac84f8a6653ff28043665862
Valerii <notused.m@gmail.com>
2024-05-16 16:22:01 +0300

    csv changes

--- adapters/clip-finance/src/index.ts
+++ adapters/clip-finance/src/index.ts
@@ -53,7 +53,7 @@ const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
 
   await new Promise<void>((resolve, reject) => {
       fs.createReadStream(filePath)
-      .pipe(csv({ separator: '\t' })) // Specify the separator as '\t' for TSV files
+      .pipe(csv()) // Specify the separator as '\t' for TSV files
       .on('data', (row) => {
          
           const blockNumber = parseInt(row.number, 10);
