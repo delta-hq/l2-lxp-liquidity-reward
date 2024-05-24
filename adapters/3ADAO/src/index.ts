fe60bf02c398f04fe9a7a33f2ae63bf4ce45eb92
CristianRicharte6 <0xcr6@proton.me>
2024-05-24 13:31:39 +0200

    Style: Logs removed

--- adapters/3ADAO/src/index.ts
+++ adapters/3ADAO/src/index.ts
@@ -40,10 +40,7 @@ export const getUserTVLByBlock = async (
   logOutput?: boolean
 ) => {
   const allCsvRows: OutputDataSchemaRow[] = []; // Array to accumulate CSV rows for all blocks
-  const batchSize = 10; // Size of batch to trigger writing to the file
-  // let i = 0;
 
-  console.log("length:", blocks.length);
   for (let i = 0; i < blocks.length; i++) {
     const { blockNumber, blockTimestamp } = blocks[i];
     try {
@@ -58,22 +55,7 @@ export const getUserTVLByBlock = async (
 
       allCsvRows.push(...csvRowsTvl);
       i++;
-      console.log(`Processed block ${i}`);
-
-      // Write to file when batch size is reached or at the end of loop
-      // if (i % batchSize === 0 || i === blocks.length) {
-      //   const ws = fs.createWriteStream(`test/outputData.csv`, {
-      //     flags: i === batchSize ? "w" : "a",
-      //   });
-      //   write(allCsvRows, { headers: i === batchSize ? true : false })
-      //     .pipe(ws)
-      //     .on("finish", () => {
-      //       console.log(`CSV file has been written.`);
-      //     });
-
-      //   // Clear the accumulated CSV rows
-      //   // allCsvRows.length = 0;
-      // }
+      // console.log(`Processed block ${i}`);
     } catch (error) {
       console.error(`An error occurred for block ${blockNumber}:`, error);
     }
