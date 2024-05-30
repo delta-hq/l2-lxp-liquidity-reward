ff3797ecbd71c92922103ab830921cbcb4e95967
Amedrontadora <steve@syncswap.xyz>
2024-05-30 23:55:00 +0800

    chore: update subgraph url support  more factory

--- adapters/syncswap/src/sdk/config.ts
+++ adapters/syncswap/src/sdk/config.ts
@@ -3,7 +3,7 @@ import { linea } from "viem/chains"
 
 export const V2_SUBGRAPH_URL = "https://gateway-arbitrum.network.thegraph.com/api/06a21853c18afff683a7ff52c764d434/subgraphs/id/3xpZFx5YNWzqemwdtRhyaTXVidKNnjY19XAWoHtvR6Lh"
 
-export const V2_SUBGRAPH_URL_AFTER_4515693 = "https://gateway-arbitrum.network.thegraph.com/api/06a21853c18afff683a7ff52c764d434/subgraphs/id/9R6uvVYXn9V1iAxkTLXL1Ajka75aD7mmHRj86DbXnyYQ"
+export const V2_SUBGRAPH_URL_AFTER_4515693 = "https://gateway-arbitrum.network.thegraph.com/api/06a21853c18afff683a7ff52c764d434/subgraphs/id/2Zv5F95PUMA95BA1b7Vzek6gtGx9kWf6XaW6ijnsVMBS"
 
 export const client = createPublicClient({
     chain: linea,
