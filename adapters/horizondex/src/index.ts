3f2aef253a56a1b05c6b3b2120045f8eae8cd7a0
HorizonDex <136569605+HorizonDex@users.noreply.github.com>
2024-05-30 11:12:27 +0400

    add horizon adapter

---
+++ adapters/horizondex/src/index.ts
@@ -0,0 +1,102 @@
+import { promisify } from 'util';
+import stream from 'stream';
+import csv from 'csv-parser';
+import fs from 'fs';
+import { write } from 'fast-csv';
+
+import { BlockData, OutputSchemaRow } from './sdk/types';
+import { getTimestampAtBlock, getV3UserPositionsAtBlock } from './sdk/lib';
+import {HORIZON_SUBGRAPH_URL, HORIZON_V2_SUBGRAPH_URL} from "./sdk/config";
+
+const pipeline = promisify(stream.pipeline);
+
+export const getUserTVLByBlock = async ({ blockNumber, blockTimestamp }: BlockData): Promise<OutputSchemaRow[]> => {
+    const result: OutputSchemaRow[] = []
+
+    const [horizonPositions, horizonV2Positions] = await Promise.all([
+        getV3UserPositionsAtBlock(blockNumber, HORIZON_SUBGRAPH_URL),
+        getV3UserPositionsAtBlock(blockNumber, HORIZON_V2_SUBGRAPH_URL)
+    ])
+
+    const combinedPositions = [...horizonPositions, ...horizonV2Positions]
+    const balances: Record<string, Record<string, bigint>> = {}
+    for (const position of combinedPositions) {
+        balances[position.user] = balances[position.user] || {}
+
+        if (position.token0.balance > 0n)
+        balances[position.user][position.token0.address] =
+            (balances?.[position.user]?.[position.token0.address] ?? 0n)
+                + position.token0.balance
+
+        if (position.token1.balance > 0n)
+        balances[position.user][position.token1.address] =
+            (balances?.[position.user]?.[position.token1.address] ?? 0n)
+                + position.token1.balance
+    }
+
+    for (const [user, tokenBalances] of Object.entries(balances)) {
+        for (const [token, balance] of Object.entries(tokenBalances)) {
+            result.push({
+                block_number: blockNumber,
+                timestamp: blockTimestamp,
+                user_address: user,
+                token_address: token,
+                token_balance: balance,
+            })
+        }
+    }
+
+    return result
+};
+
+const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
+    const blocks: BlockData[] = [];
+
+    await new Promise<void>((resolve, reject) => {
+      fs.createReadStream(filePath)
+        .pipe(csv()) // Specify the separator as '\t' for TSV files
+        .on('data', (row) => {
+          const blockNumber = parseInt(row.number, 10);
+          const blockTimestamp = parseInt(row.timestamp, 10);
+          if (!isNaN(blockNumber) && blockTimestamp) {
+            blocks.push({ blockNumber: blockNumber, blockTimestamp });
+          }
+        })
+        .on('end', () => {
+          resolve();
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+
+    return blocks;
+  };
+
+
+  readBlocksFromCSV('hourly_blocks.csv').then(async (blocks: any[]) => {
+    console.log(blocks);
+    const allCsvRows: any[] = []; // Array to accumulate CSV rows for all blocks
+
+    for (const block of blocks) {
+        try {
+            const result = await getUserTVLByBlock(block);
+            // Accumulate CSV rows for all blocks
+            allCsvRows.push(...result);
+        } catch (error) {
+            console.error(`An error occurred for block ${block}:`, error);
+        }
+    }
+    await new Promise((resolve, reject) => {
+      const ws = fs.createWriteStream(`outputData.csv`, { flags: 'w' });
+      write(allCsvRows, { headers: true })
+          .pipe(ws)
+          .on("finish", () => {
+          console.log(`CSV file has been written.`);
+          resolve;
+          });
+    });
+
+  }).catch((err) => {
+    console.error('Error reading CSV file:', err);
+  });
---
+++ adapters/horizondex/src/sdk/lib.ts
@@ -0,0 +1,145 @@
+import { HORIZON_SUBGRAPH_URL, HORIZON_V2_SUBGRAPH_URL, client } from "./config"
+import { UserPosition } from "./types"
+
+type V3Position = {
+    liquidity: string,
+    owner: string,
+    pool: {
+        sqrtPrice: string,
+        tick: string,
+        token0: {
+            id: string,
+            symbol: string,
+        },
+        token1: {
+            id: string,
+            symbol: string
+        }
+        token0Price: string,
+        token1Price: string
+    }
+    tickLower: {
+        tickIdx: string
+    },
+    tickUpper: {
+        tickIdx: string
+    },
+}
+
+const getV3PositionReserves = (position: V3Position) => {
+    const liquidity = +position.liquidity
+    const _sqrtPrice = +position.pool.sqrtPrice
+    const currentTick = +position.pool.tick
+    const tickLower = +position.tickLower.tickIdx
+    const tickUpper = +position.tickUpper.tickIdx
+
+    let reserve0 = 0n
+    let reserve1 = 0n
+
+    if (liquidity === 0) {
+        return {
+          reserve0,
+          reserve1,
+        }
+    }
+
+    const sqrtRatioA = Math.sqrt(1.0001 ** tickLower)
+    const sqrtRatioB = Math.sqrt(1.0001 ** tickUpper)
+    const sqrtPrice = _sqrtPrice / (2 ** 96)
+
+    if (currentTick >= tickLower && currentTick < tickUpper) {
+        reserve0 = BigInt(Math.floor(liquidity * ((sqrtRatioB - sqrtPrice) / (sqrtPrice * sqrtRatioB))))
+        reserve1 = BigInt(Math.floor(liquidity * (sqrtPrice - sqrtRatioA)))
+    }
+
+    return {
+        reserve0,
+        reserve1
+    }
+}
+
+export const getV3UserPositionsAtBlock = async (blockNumber: number, subgraphUrl: string): Promise<UserPosition[]> => {
+    const result: UserPosition[] = []
+
+    let skip = 0
+    let fetchNext = true
+    while (fetchNext) {
+        const query = `query {
+            positions(
+                first: 1000,
+                skip: ${skip},
+                where: { liquidity_gt: 0 },
+                block: { number: ${blockNumber} }
+            ) {
+                liquidity
+                owner
+                pool {
+                    sqrtPrice
+                    tick
+                    token0 {
+                        id
+                        symbol
+                    }
+                    token1 {
+                        id
+                        symbol
+                    }
+                    token0Price
+                    token1Price
+                }
+                tickLower {
+                    tickIdx
+                }
+                tickUpper {
+                    tickIdx
+                }
+            }
+        }`
+
+        const response = await fetch(subgraphUrl, {
+            method: "POST",
+            body: JSON.stringify({ query }),
+            headers: { "Content-Type": "application/json" },
+        })
+
+        const { data, errors } = await response.json();
+        if (errors) {
+            return result
+        }
+        const { positions } = data
+
+        result.push(...positions.map((position: V3Position) => {
+            const { reserve0, reserve1 } = getV3PositionReserves(position)
+            return {
+                user: position.owner,
+                token0: {
+                    address: position.pool.token0.id,
+                    balance: reserve0,
+                    symbol: position.pool.token0.symbol,
+                    usdPrice: +position.pool.token0Price
+                },
+                token1: {
+                    address: position.pool.token1.id,
+                    balance: reserve1,
+                    symbol: position.pool.token1.symbol,
+                    usdPrice: +position.pool.token1Price
+                }
+            }
+        }))
+
+        if (positions.length < 1000) {
+            fetchNext = false;
+        } else {
+            skip += 1000;
+        }
+    }
+
+    return result
+}
+
+export const getTimestampAtBlock = async (blockNumber: number) => {
+    const block = await client.getBlock({
+        blockNumber: BigInt(blockNumber),
+    });
+    return Number(block.timestamp * 1000n);
+};
