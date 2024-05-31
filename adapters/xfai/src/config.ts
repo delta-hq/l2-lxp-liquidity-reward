d342bb1d5ce62aa5f94563af0989a580fa572ff4
0xroll <96259407+0xroll@users.noreply.github.com>
2024-05-31 12:05:48 +0800

    Revert "[xfai] switch to thegraph, track transfer events"

--- adapters/xfai/src/config.ts
+++ adapters/xfai/src/config.ts
@@ -1,10 +1,9 @@
 export const CHAIN_ID = 59144;
 export const RPC_URL = "https://rpc.linea.build";
+export const LIQUIDITY_EVENTS_DB =
+  "postgres://mobula_readonly:password@analytics.cb5emp3h2kdo.eu-central-1.rds.amazonaws.com:5432/xfai-logs?sslmode=no-verify";
 export const XFAI_FACTORY = "0xa5136eAd459F0E61C99Cec70fe8F5C24cF3ecA26";
 export const XFAI_POOL_INIT =
   "0xd29425d309539268aa2f934062f86ea332822e787dafc6baba7cfda029630330";
 export const MULTICALL = "0xca11bde05977b3631167028862be2a173976ca11";
 export const WETH = "0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f";
-
-export const SUBGRAPH_URL =
-  "https://api.studio.thegraph.com/query/68034/xfai-dex/v1.0.0";
