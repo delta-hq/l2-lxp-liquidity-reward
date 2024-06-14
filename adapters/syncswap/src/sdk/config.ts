5bd02f45b175db9ffa6e4e9979cc24cbd7e43df8
Amedrontadora <steve@syncswap.xyz>
2024-06-15 02:32:24 +0800

    SyncSwap: fix missing data issue

--- adapters/syncswap/src/sdk/config.ts
+++ adapters/syncswap/src/sdk/config.ts
@@ -1,7 +1,7 @@
 import { createPublicClient, http } from "viem";
 import { linea } from "viem/chains"
 
-export const V2_SUBGRAPH_URL = "https://gateway-arbitrum.network.thegraph.com/api/06a21853c18afff683a7ff52c764d434/deployments/id/QmT8aCpWka6j2maHmedHQGtu13ru6tx7avsT8Y9tNYrP7s"
+export const V2_SUBGRAPH_URL = "https://gateway-arbitrum.network.thegraph.com/api/06a21853c18afff683a7ff52c764d434/deployments/id/QmdCXFD4FXbP7zJoFMPGHULyafWnpTN5DHUDJw41QVUp9G"
 
 export const client = createPublicClient({
     chain: linea,
