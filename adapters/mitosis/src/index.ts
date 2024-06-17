a4206b672baddf650a5b607b1dacaccc0f093a7a
byeongsu-hong <hong@byeongsu.dev>
2024-06-18 02:29:04 +0900

    apply checklist

--- adapters/mitosis/src/index.ts
+++ adapters/mitosis/src/index.ts
@@ -1,10 +1,14 @@
+import fs from 'fs';
+import { write } from "fast-csv";
+import csv from "csv-parser";
+
 const TOKEN_ADDRESS = "0x3478de5e82431676c87113001bbeeb359cb5eaa5";
 const TOKEN_SYMBOL = 'miweETH';
 
 const GRAPHQL_ENDPOINT = "https://api.goldsky.com/api/public/project_clxioqhjdzy1901wmgqmp2ygj/subgraphs/mitosis-linea-lxp/1.0.0/gn";
 
-const makeQuery = (blockNumber: number) => `query MyQuery {
-  tokenBalances(block: {number: ${blockNumber}}) {
+const makeQuery = (blockNumber: number, next = "") => `query balance {
+  tokenBalances(block: {number: ${blockNumber}}, first: 1000, where: { id_gt: "${next}" }) {
     value
     id
   }
@@ -46,12 +50,8 @@ async function post<T = any>(url: string, data: any): Promise<{ data: T }> {
   return await response.json();
 };
 
-export const getUserTVLByBlock = async (blocks: BlockData): Promise<OutputDataSchemaRow[]> => {
-  const { blockNumber, blockTimestamp } = blocks
-
-  const { data: { tokenBalances } } = await post<TokenBalancesResponse>(GRAPHQL_ENDPOINT, makeQuery(blockNumber));
-
-  const output = tokenBalances.map((v) => ({
+const toOutput = ({ blockNumber, blockTimestamp }: BlockData, { tokenBalances }: TokenBalancesResponse): OutputDataSchemaRow[] =>
+  tokenBalances.map((v) => ({
     block_number: blockNumber,
     timestamp: blockTimestamp,
     user_address: v.id,
@@ -59,7 +59,69 @@ export const getUserTVLByBlock = async (blocks: BlockData): Promise<OutputDataSc
     token_balance: BigInt(v.value),
     token_symbol: TOKEN_SYMBOL,
     usd_price: 0
-  }));
+  }))
+
+export const getUserTVLByBlock = async (blocks: BlockData): Promise<OutputDataSchemaRow[]> => {
+  let next = "";
+  let output: OutputDataSchemaRow[] = [];
+
+  while (true) {
+    const { data: resp } = await post<TokenBalancesResponse>(GRAPHQL_ENDPOINT, makeQuery(blocks.blockNumber, next));
+    if (resp.tokenBalances.length === 0) break;
+
+    output = output.concat(toOutput(blocks, resp));
+    next = resp.tokenBalances[resp.tokenBalances.length - 1].id;
+  }
 
   return output;
 };
+
+const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
+  const blocks: BlockData[] = [];
+
+  await new Promise<void>((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv()) // Specify the separator as '\t' for TSV files
+      .on('data', (row) => {
+        const blockNumber = parseInt(row.number, 10);
+        const blockTimestamp = parseInt(row.timestamp, 10);
+        if (!isNaN(blockNumber) && blockTimestamp) {
+          blocks.push({ blockNumber: blockNumber, blockTimestamp });
+        }
+      })
+      .on('end', () => {
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+
+  return blocks;
+};
+
+readBlocksFromCSV('hourly_blocks.csv').then(async (blocks: any[]) => {
+  console.log(blocks);
+  const allCsvRows: any[] = [];
+
+  for (const block of blocks) {
+    try {
+      const result = await getUserTVLByBlock(block);
+      allCsvRows.push(...result);
+    } catch (error) {
+      console.error(`An error occurred for block ${block}:`, error);
+    }
+  }
+  await new Promise((resolve, reject) => {
+    const ws = fs.createWriteStream(`outputData.csv`, { flags: 'w' });
+    write(allCsvRows, { headers: true })
+      .pipe(ws)
+      .on("finish", () => {
+        console.log(`CSV file has been written.`);
+        resolve;
+      });
+  });
+
+}).catch((err) => {
+  console.error('Error reading CSV file:', err);
+});
