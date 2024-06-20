23cdd255de0ef99057a3e3bb3764655cfa69f70a
batphonghan <vanhungdeveloper@gmail.com>
2024-06-20 20:59:57 +0700

    wrsETH + rsETH tracking

---
+++ adapters/rsETH_linea/src/query.ts
@@ -0,0 +1,81 @@
+import { request } from "graphql-request";
+import { gql } from "graphql-request";
+
+const MULTICALL_BATCH_SIZE = 1000;
+
+const LINEA_RSETH_SUBGRAPH =
+  "https://api.thegraph.com/subgraphs/id/QmYU2LVKDDQTztMYVHf6tSrPYGx9TdNkad4jYTCL22XtfE";
+export const LINEA_RSETH_ADDR = "0x4186BFC76E2E237523CBC30FD220FE055156b41F";
+export const LINEA_RSETH = "rsETH";
+interface IDwise {
+  id: string;
+}
+
+async function subgraphFetchAllById<T extends IDwise>(
+  endpoint: string,
+  query: string,
+  collection: string,
+  variables: Record<string, unknown>
+): Promise<T[]> {
+  const data: T[] = [];
+  let lastId = "0x0000000000000000000000000000000000000000";
+  while (true) {
+    const resp: { [collection: string]: T[] } = await request(endpoint, query, {
+      ...variables,
+      lastId
+    });
+
+    const batch: T[] = resp[collection];
+    if (batch.length == 0) {
+      break;
+    }
+
+    const last = batch[batch.length - 1];
+    lastId = last.id;
+
+    data.push(...batch);
+
+    if (batch.length < MULTICALL_BATCH_SIZE) {
+      break;
+    }
+  }
+  return data;
+}
+
+interface GraphQLQuery {
+  query: string;
+  collection: string;
+}
+
+export type UserBalanceSubgraphEntry = {
+  id: string;
+  balance: string;
+};
+
+export const USER_BALANCES_QUERY: GraphQLQuery = {
+  query: gql`
+    query PositionsQuery($block: Int, $lastId: ID!) {
+      userBalances(
+        where: { balance_gt: "0", id_gt: $lastId }
+        block: { number: $block }
+        orderBy: id
+        orderDirection: asc
+        first: 1000
+      ) {
+        id
+        balance
+      }
+    }
+  `,
+  collection: "userBalances"
+};
+
+export async function getAllRsEthHodlers(blockNumber: number) {
+  const positions = await subgraphFetchAllById<UserBalanceSubgraphEntry>(
+    LINEA_RSETH_SUBGRAPH,
+    USER_BALANCES_QUERY.query,
+    USER_BALANCES_QUERY.collection,
+    { block: blockNumber, lastId: "0x0000000000000000000000000000000000000000" }
+  );
+  return positions;
+}
