949c95ada1eeb81c7af95d519331785149ce58ed
batphonghan <vanhungdeveloper@gmail.com>
2024-09-11 11:51:06 +0700

    Add spectra

--- adapters/kelp_gain_linea/src/lib/utils.ts
+++ adapters/kelp_gain_linea/src/lib/utils.ts
@@ -23,6 +23,7 @@ const rsEthAbi = [
 ];
 
 const agEthAbi = [
+  "function convertToShares(uint256 shares) public view returns (uint256)",
   "function convertToAssets(uint256 shares) public view returns (uint256)",
   "function totalSupply() public view returns (uint256)",
   "function balanceOf(address account) public view returns (uint256)"
