e9a1b92e72ea9b6ceb31920004e9d8a8fd3cf34e
ultrasecr.eth <241804+ultrasecreth@users.noreply.github.com>
2024-09-23 16:48:11 +0100

    Adhere to README

--- adapters/contango/src/index.ts
+++ adapters/contango/src/index.ts
@@ -2,63 +2,15 @@ import csv from "csv-parser";
 import fs from "fs";
 import { write } from "fast-csv";
 
-const Linea = 59_144;
-
-type PoolSchemaRow = {
-  chain_id: number;
-  creation_block_number: number;
+type OutputDataSchemaRow = {
+  block_number: number;
   timestamp: number;
-  underlying_token_address: string;
-  underlying_token_symbol: string;
-  receipt_token_address: string;
-  receipt_token_symbol: string;
-  pool_address: string;
-  pool_type: string;
-};
-
-type PositionSnapshotSchemaRow = {
-  timestamp: number;
-  block_date: string;
-  chain_id: number;
-  pool_address: string;
-  underlying_token_address: string;
-  underlying_token_symbol: string;
   user_address: string;
-  supplied_amount: bigint;
-  supplied_amount_usd?: bigint;
-  borrowed_amount: bigint;
-  borrowed_amount_usd?: bigint;
-  collateral_amount?: bigint;
-  collateral_amount_usd?: bigint;
+  token_address: string;
+  token_balance: bigint;
+  token_symbol: string;
+  usd_price?: number;
 };
-
-type PoolSnapshotSchemaRow = {
-  timestamp: number;
-  block_date: string;
-  chain_id: number;
-  pool_address: string;
-  underlying_token_address: string;
-  underlying_token_symbol: string;
-  underlying_token_price_usd?: number;
-  available_amount: bigint;
-  available_amount_usd?: bigint;
-  supplied_amount: bigint;
-  supplied_amount_usd?: bigint;
-  non_recursive_supplied_amount: bigint;
-  collateral_amount?: bigint;
-  collateral_amount_usd?: bigint;
-  collateral_factor: number;
-  supply_index: number;
-  supply_apr: number;
-  borrowed_amount: bigint;
-  borrowed_amount_usd?: bigint;
-  borrow_index: number;
-  borrow_apr: number;
-  total_fees_usd?: number;
-  user_fees_usd?: number;
-  protocol_fees_usd?: number;
-};
-
 interface BlockData {
   blockNumber: number;
   blockTimestamp: number;
@@ -136,183 +88,68 @@ const queryAllPositions = async (block: number): Promise<Position[]> => {
   return positions;
 };
 
-const queryAllAssets = async (block: number): Promise<Asset[]> => {
-  const query = async (
-    block: number,
-    first: number,
-    skip: number,
-  ): Promise<Asset[]> => {
-    const query = `query ContangoAssets {
-      assets(
-        block: {number: ${block}}
-        first: ${first}
-        skip: ${skip}
-      ) {
-        id
-        symbol
-      }
-    }`;
+type User = string;
+type AssetAddress = string;
+enum Side {
+  Collateral,
+  Debt,
+}
 
-    return await graphQuery(query).then((json) => json.data.assets);
+const row = (
+  block: BlockData,
+  owner: User,
+  asset: Asset,
+): OutputDataSchemaRow => {
+  return {
+    block_number: block.blockNumber,
+    timestamp: block.blockTimestamp,
+    user_address: owner,
+    token_address: asset.id,
+    token_balance: 0n,
+    token_symbol: asset.symbol,
   };
-
-  let assets: Asset[] = [];
-  let skip = 0;
-  do {
-    assets.push(...(await query(block, PAGE_SIZE, skip)));
-    skip += PAGE_SIZE;
-  } while (assets.length === PAGE_SIZE);
-
-  return assets;
 };
 
-function formatDate(date: Date): string {
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-
-  return `${year}-${month}-${day} ${hours}:00:00`;
-}
-
-export const getPoolsByBlock = async (
+export const getUserTVLByBlock = async (
   block: BlockData,
-): Promise<PoolSchemaRow[]> => {
-  const assets = await queryAllAssets(block.blockNumber);
-  return assets.map(({ id, symbol }) => {
-    return {
-      chain_id: Linea,
-      creation_block_number: 0,
-      timestamp: 0,
-      underlying_token_address: id,
-      underlying_token_symbol: symbol,
-      receipt_token_address: "",
-      receipt_token_symbol: "",
-      pool_address: id,
-      pool_type: "isolated",
-    };
-  });
-};
+): Promise<OutputDataSchemaRow[]> => {
+  const positions = await queryAllPositions(block.blockNumber);
+  console.log(
+    `Processing block ${block.blockNumber} with ${positions.length} positions`,
+  );
 
-type Pool = string
-type User = string
-
-export const getPositionSnapshotsByBlock = async (
-  block: BlockData,
-): Promise<PositionSnapshotSchemaRow[]> => {
-  const { blockNumber, blockTimestamp } = block;
-  const positions = await queryAllPositions(blockNumber);
-
-  const positionsByUser = new Map<User, Map<Pool, PositionSnapshotSchemaRow>>();
+  const positionsByUser = new Map<User, Map<AssetAddress, Map<Side,OutputDataSchemaRow >>>();
 
   positions.forEach((position) => {
     const { owner, collateral, debt, instrument } = position;
     const { base, quote } = instrument;
 
-    if(!positionsByUser.has(owner)) {
-      positionsByUser.set(owner, new Map<Pool, PositionSnapshotSchemaRow>());
+    if (!positionsByUser.has(owner)) {
+      positionsByUser.set(owner, new Map<AssetAddress, Map<Side,OutputDataSchemaRow >>());
     }
-    const userPositions = positionsByUser.get(owner)!;
+    const userAssets = positionsByUser.get(owner)!;
 
-    if (!userPositions.has(base.id)) {
-      userPositions.set(base.id, {
-        timestamp: blockTimestamp,
-        block_date: formatDate(new Date(blockTimestamp * 1000)),
-        chain_id: Linea,
-        pool_address: base.id,
-        underlying_token_address: base.id,
-        underlying_token_symbol: base.symbol,
-        user_address: owner,
-        supplied_amount: BigInt(collateral),
-        borrowed_amount: 0n,
-      });
-    } else {
-      userPositions.get(base.id)!.supplied_amount += BigInt(collateral);
+    if (!userAssets.has(base.id)) {
+      userAssets.set(base.id, new Map<Side,OutputDataSchemaRow >());
+    }
+    if (!userAssets.has(quote.id)) {
+      userAssets.set(quote.id, new Map<Side,OutputDataSchemaRow >());
+    }
+    if (!userAssets.get(base.id)!.has(Side.Collateral)) {
+      userAssets.get(base.id)!.set(Side.Collateral, row(block, owner, base));
+    }
+    if (!userAssets.get(quote.id)!.has(Side.Debt)) {
+      userAssets.get(quote.id)!.set(Side.Debt, row(block, owner, quote));
     }
 
-    if (!userPositions.has(quote.id)) {
-      userPositions.set(quote.id, {
-        timestamp: blockTimestamp,
-        block_date: formatDate(new Date(blockTimestamp * 1000)),
-        chain_id: Linea,
-        pool_address: quote.id,
-        underlying_token_address: quote.id,
-        underlying_token_symbol: quote.symbol,
-        user_address: owner,
-        supplied_amount: 0n,
-        borrowed_amount: BigInt(debt),
-      });
-    } else {
-      userPositions.get(quote.id)!.borrowed_amount += BigInt(debt);
-    }
+    userAssets.get(base.id)!.get(Side.Collateral)!.token_balance += BigInt(collateral);
+    userAssets.get(quote.id)!.get(Side.Debt)!.token_balance -= BigInt(debt);
   });
 
-  return Array.from(positionsByUser.values()).flatMap((positions) => Array.from(positions.values()));
+  return Array.from(positionsByUser.values())
+    .flatMap((positions) => Array.from(positions.values()))
+    .flatMap((sides) => Array.from(sides.values()))
 };
-
-export const getPoolSnapshotsByBlock = async (
-  block: BlockData,
-): Promise<PoolSnapshotSchemaRow[]> => {
-  const { blockNumber, blockTimestamp } = block;
-  const positions = await queryAllPositions(blockNumber);
-
-  const results = new Map<string, PoolSnapshotSchemaRow>();
-
-  positions.forEach((position) => {
-    const { owner, collateral, debt, instrument } = position;
-    const { base, quote } = instrument;
-
-    if (!results.has(base.id)) {
-      results.set(base.id, {
-        timestamp: blockTimestamp,
-        block_date: formatDate(new Date(blockTimestamp * 1000)),
-        chain_id: Linea,
-        pool_address: base.id,
-        underlying_token_address: base.id,
-        underlying_token_symbol: base.symbol,
-        available_amount: 0n,
-        supplied_amount: BigInt(collateral),
-        non_recursive_supplied_amount: BigInt(collateral),
-        collateral_factor: 0,
-        supply_index: 0,
-        supply_apr: 0,
-        borrowed_amount: 0n,
-        borrow_index: 0,
-        borrow_apr: 0,
-
-      });
-    } else {
-      const pool = results.get(base.id)!;
-      pool.supplied_amount += BigInt(collateral)
-      pool.non_recursive_supplied_amount += BigInt(collateral)
-    }
-
-    if (!results.has(quote.id)) {
-      results.set(quote.id, {
-        timestamp: blockTimestamp,
-        block_date: formatDate(new Date(blockTimestamp * 1000)),
-        chain_id: Linea,
-        pool_address: quote.id,
-        underlying_token_address: quote.id,
-        underlying_token_symbol: quote.symbol,
-        available_amount: 0n,
-        supplied_amount: 0n,
-        non_recursive_supplied_amount: 0n,
-        collateral_factor: 0,
-        supply_index: 0,
-        supply_apr: 0,
-        borrowed_amount: BigInt(debt),
-        borrow_index: 0,
-        borrow_apr: 0,
-      });
-    } else {
-      const pool = results.get(quote.id)!;
-      pool.borrowed_amount += BigInt(debt);
-    }
-  });
-
-  return Array.from(results.values());
-}
 
 const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
   const blocks: BlockData[] = [];
@@ -338,41 +175,36 @@ const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
   return blocks;
 };
 
-const main = async (fileName: string, fn: (block: BlockData) => Promise<any>) =>
-  readBlocksFromCSV("hourly_blocks.csv").then(async (blocks: any[]) => {
-    console.log(`Processing ${blocks.length} blocks for ${fileName}...`);
+readBlocksFromCSV("hourly_blocks.csv")
+  .then(async (blocks: any[]) => {
     console.log(blocks);
-    const allCsvRows: any[] = [];
+    const allCsvRows: any[] = []; // Array to accumulate CSV rows for all blocks
+    const batchSize = 1000; // Size of batch to trigger writing to the file
     let i = 0;
 
     for (const block of blocks) {
-      const blockNumber = block.blockNumber;
-      if (!blockNumber || blockNumber < 8030000) {
-        throw new Error(`Block number ${blockNumber} is invalid`);
-      }
-
       try {
-        const rows = await fn(block);
-        console.log(`Block ${blockNumber} has ${rows.length} rows.`);
-
-        allCsvRows.push(...rows);
+        const result = await getUserTVLByBlock(block);
+        allCsvRows.push(...result);
       } catch (error) {
         console.error(`An error occurred for block ${block}:`, error);
       }
     }
-
     await new Promise((resolve, reject) => {
-      const ws = fs.createWriteStream(fileName, { flags: "w" });
+      // const randomTime = Math.random() * 1000;
+      // setTimeout(resolve, randomTime);
+      const ws = fs.createWriteStream(`outputData.csv`, { flags: "w" });
       write(allCsvRows, { headers: true })
         .pipe(ws)
         .on("finish", () => {
-          console.log(`CSV [${allCsvRows.length}] file has been written.`);
+          console.log(`CSV file has been written.`);
           resolve;
         });
     });
-  });
 
-console.log("Starting...");
-main("pools.csv", getPoolsByBlock)
-main("positionSnapshots.csv", getPositionSnapshotsByBlock)
-main("poolSnapshots.csv", getPoolSnapshotsByBlock)
+    // Clear the accumulated CSV rows
+    // allCsvRows.length = 0;
+  })
+  .catch((err) => {
+    console.error("Error reading CSV file:", err);
+  });
