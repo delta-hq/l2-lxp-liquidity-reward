61303a636860be325503ea45703ae946f301d4ce
Apeguru <crypto.ape.guru@gmail.com>
2024-10-07 18:24:30 +0700

    chore: Update RPC urls

--- adapters/lynex/src/sdk/config.ts
+++ adapters/lynex/src/sdk/config.ts
@@ -2,9 +2,9 @@ import { createPublicClient, http } from "viem";
 import { linea } from "viem/chains";
 
 export const V2_SUBGRAPH_URL =
-  "https://api.studio.thegraph.com/query/59052/lynex-v1/version/latest";
+  "https://api.goldsky.com/api/public/project_cltyhthusbmxp01s95k9l8a1u/subgraphs/lynex-v1/1.0.4/gn";
 export const GAUGE_SUBGRAPH_URL =
-  "https://api.goldsky.com/api/public/project_cltyhthusbmxp01s95k9l8a1u/subgraphs/lynex-gauges/1.1.0/gn";
+  "https://api.goldsky.com/api/public/project_cltyhthusbmxp01s95k9l8a1u/subgraphs/lynex-gauges/1.2.4/gn";
 export const V3_SUBGRAPH_URL =
   "https://api.goldsky.com/api/public/project_cltyhthusbmxp01s95k9l8a1u/subgraphs/lynex-cl/v1.0.2/gn";
 
