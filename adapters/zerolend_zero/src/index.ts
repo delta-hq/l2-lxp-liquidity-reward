ee0a90e5faae619e0eea2117af0a227e918876a5
jh <0xroll.onchain@gmail.com>
2024-11-08 11:33:49 +0800

    zerolend with staker details only

---
+++ adapters/zerolend_zero/src/index.ts
@@ -0,0 +1,75 @@
+import { write } from "fast-csv";
+import fs from "fs";
+import csv from "csv-parser";
+import { BlockData, OutputDataSchemaRow } from "./sdk/types";
+import { getUserTVLLegacyByBlock } from "./sdk/tvl";
+import { getUserStakeByBlock } from "./sdk/stake";
+import { getUserLPByBlock } from "./sdk/lp";
+import { getUserTVLFoxyByBlock } from "./sdk/foxy";
+
+const readBlocksFromCSV = async (filePath: string): Promise<BlockData[]> => {
+  const blocks: BlockData[] = [];
+
+  await new Promise<void>((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv()) // Specify the separator as '\t' for TSV files
+      .on("data", (row) => {
+        const blockNumber = parseInt(row.number, 10);
+        const blockTimestamp = parseInt(row.timestamp, 10);
+        if (!isNaN(blockNumber) && blockTimestamp) {
+          blocks.push({ blockNumber: blockNumber, blockTimestamp });
+        }
+      })
+      .on("end", () => {
+        resolve();
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+
+  return blocks;
+};
+
+readBlocksFromCSV("hourly_blocks.csv")
+  .then(async (blocks: BlockData[]) => {
+    console.log(blocks);
+    let allCsvRows: OutputDataSchemaRow[] = []; // Array to accumulate CSV rows for all blocks
+
+    for (const block of blocks) {
+      try {
+        const data = await getUserTVLByBlock(block);
+        allCsvRows = allCsvRows.concat(data);
+      } catch (error) {
+        console.error(`An error occurred for block ${block}:`, error);
+      }
+    }
+    await new Promise((resolve, reject) => {
+      const ws = fs.createWriteStream(`outputData.csv`, { flags: "w" });
+      write(allCsvRows, { headers: true })
+        .pipe(ws)
+        .on("finish", () => {
+          console.log(`CSV file has been written.`);
+          resolve;
+        });
+    });
+  })
+  .catch((err) => {
+    console.error("Error reading CSV file:", err);
+  });
+
+const getUserTVLByBlock = async (block: BlockData): Promise<any> => {
+  let allCsvRows: OutputDataSchemaRow[] = []; // Array to accumulate CSV rows for all blocks
+
+  const resultStake = await getUserStakeByBlock(block);
+  allCsvRows = allCsvRows.concat(resultStake);
+
+  const resultLp = await getUserLPByBlock(block);
+  allCsvRows = allCsvRows.concat(resultLp);
+
+  return allCsvRows;
+};
+
+module.exports = {
+  getUserTVLByBlock,
+};
---
+++ adapters/zerolend_zero/src/sdk/stake.ts
@@ -0,0 +1,76 @@
+import axios from "axios";
+import rateLimit from "axios-rate-limit";
+import {
+  BlockData,
+  IOmniStakingData,
+  IOmniStakingResponse,
+  OutputDataSchemaRow,
+} from "./types";
+
+const axiosInstance = rateLimit(axios.create(), {
+  maxRequests: 5,
+  perMilliseconds: 1000,
+});
+
+const queryURL =
+  "https://api.goldsky.com/api/public/project_clsk1wzatdsls01wchl2e4n0y/subgraphs/zerolend-omnistaking/1.0.2/gn";
+
+const tokenAddress = "0x78354f8dccb269a615a7e0a24f9b0718fdc3c7a7"; //do we need to convert the case
+const symbol = "ZERO";
+
+export const getUserStakeByBlock = async (
+  blocks: BlockData
+): Promise<OutputDataSchemaRow[]> => {
+  const timestamp = blocks.blockTimestamp;
+  const first = 1000;
+  const rows: OutputDataSchemaRow[] = [];
+
+  let lastAddress = "0x0000000000000000000000000000000000000000";
+  console.log("working on ZERO stakers data");
+  do {
+    const query = `{
+      tokenBalances(
+        where: {
+          id_gt: "${lastAddress}",
+          balance_omni_gt: "0"
+        }
+        first: ${first}
+      ) {
+        id
+        balance_omni
+      }
+    }`;
+
+    const response = await axiosInstance.post(
+      queryURL,
+      { query },
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    const batch: IOmniStakingResponse = await response.data;
+
+    if (!batch.data || batch.data.tokenBalances.length == 0) break;
+
+    batch.data.tokenBalances.forEach((data: IOmniStakingData) => {
+      rows.push({
+        block_number: blocks.blockNumber,
+        timestamp,
+        user_address: data.id,
+        token_address: tokenAddress,
+        token_balance: BigInt(data.balance_omni),
+        token_symbol: symbol,
+        usd_price: 0,
+      });
+
+      lastAddress = data.id;
+    });
+
+    console.log(
+      `Processed ${rows.length} rows for single stakers. Last address is ${lastAddress}`
+    );
+  } while (true);
+
+  return rows.filter((r) => r.token_balance > 1);
+};
